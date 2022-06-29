33f5ee37898908560997148d8bd20d24f6918447
Shaun <ztlguswkdz@naver.com>
2022-06-29 22:26:04 +0900

    13 몬스터 로밍구현

--- Assets/Scripts/Main/Monster.cs
+++ Assets/Scripts/Main/Monster.cs
@@ -8,16 +8,23 @@ public class Monster : MonoBehaviour
     {
         CREATE, IDLE, BATTLE, DIE,ROAMING
     }
+    public float speed = 3f;
     public State myState = State.CREATE;
     public int maxHp;
     public int curHp;
     public int Damage;
     public Renderer myrenderer;
     public Animator myanim;
-
+    public Vector3 pos;
+    public LayerMask targetmask;
+    Vector3 TargetPos = Vector3.zero;
+    public Collider[] targetIndistance;
+    public GameObject myTarget;
+    public float Attackrange;
     private void Awake()
     {
         ChangeState(State.IDLE);
+
        
     }
     private void OnTriggerEnter(Collider other)//트리거엔터가 발동될때 
@@ -37,7 +44,13 @@ public class Monster : MonoBehaviour
     }
     IEnumerator OnDamage() // ondamage코루틴 
     {
-
+        targetIndistance = Physics.OverlapSphere(transform.position, 20f, targetmask);
+        myTarget = targetIndistance[0].gameObject;
+        if (myTarget != null)
+        {
+            ChangeState(State.BATTLE);
+            
+        }
         myrenderer.material.color= Color.red; // 몬스터 색깔 빨강으로 변경 
         yield return new WaitForSeconds(0.2f); // 0.2 초 기다린뒤 
         if(curHp>0)// 현재 hp 가 0보다크다면 
@@ -68,7 +81,71 @@ public class Monster : MonoBehaviour
         }
         Destroy(this.gameObject);// while 문 끝난후 몬스터 삭제 
     }
+    private void Update()
+    {
+       
+    }
+    IEnumerator Battle()
+    {
+        pos = myTarget.transform.position;
+        myanim.SetBool("IsRunnig", true);
+        while (true)
+        {
+            var dir = (pos - this.transform.position).normalized;
+            this.transform.LookAt(pos);
+            this.transform.position += dir * speed * Time.deltaTime;
 
+            float distance = Vector3.Distance(transform.position, pos);
+            if(distance<= Attackrange)
+            {
+                if(myanim.GetBool("IsAttack")==false)
+                {
+                    myanim.SetTrigger("Attack");
+                }
+                
+
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator Roaming()
+    {
+        
+
+        pos = new Vector3(); //목적지 생성
+        pos.x = Random.Range(-3f, 3f); //목적지 x 값은 -3~3 사이 랜덤값
+        pos.z = Random.Range(-3f, 3f); // 목적지 z 값은 -3~3 사이 랜덤값
+       
+
+        myanim.SetBool("IsWalking", true); // 처음에 움직이니까 트루 
+        while (true) 
+        {
+           
+            var dir = (pos - this.transform.position).normalized; // pos 포지션 - 몬스터포지션 normalized 한후 
+            this.transform.LookAt(pos); // 몬스터가 이동할때 이동하는곳 바라보게하기위해 
+            this.transform.position += dir * speed * Time.deltaTime; // 현재포지션에 normalize * 설정한 speed * Time.deltaTime 더해주기 
+
+            float distance = Vector3.Distance(transform.position, pos); // 몬스터와 목적지 사이 거리 구하기 
+            if (distance <=0.1f) // 0.1 이하라면 
+            {
+                myanim.SetBool("IsWalking", false); // 걷는 모션 false 후 Idle 들어갈예정 
+                yield return new WaitForSeconds(Random.Range(1f, 3f)); // 랜덤으로 1~3 초기다린후 
+                myanim.SetBool("IsWalking", true); // 다시 걷게하고 
+                pos.x = Random.Range(-3f, 3f);// 위치설정
+                pos.z = Random.Range(-3f, 3f);//위치설정 
+               
+
+            }
+            
+            yield return null;  
+        }
+
+       
+       
+    
+
+    }
     void ChangeState(State s) // State 변경 
     {
 
@@ -80,10 +157,16 @@ public class Monster : MonoBehaviour
             case State.CREATE:
                 break;
             case State.IDLE:
+                ChangeState(State.ROAMING);
                 break;
             case State.ROAMING:
+                speed = 3f;
+                StartCoroutine(Roaming());
                 break;
             case State.BATTLE:
+                speed = 5f;
+                StopCoroutine(Roaming());
+                StartCoroutine(Battle());
                 break;
             case State.DIE: // die 상태로돌입하면 
                
